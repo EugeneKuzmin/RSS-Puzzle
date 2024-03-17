0d05a079014e8bc32c4f06c0eae27afbf454e415
Gennarro@yandex.ru <Gennarro@yandex.ru>
2024-03-17 20:35:46 +0300

    refactor:move input create logic into a function

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -2,40 +2,51 @@ import './LoginStyles.css';
 import validateInput from '../utils/validation'
 import BaseHTMLElementClass from '../BaseHTMLElementClass';
 const errorMsg = {
-    textName: 'Please write a name in latin 3 letters minimum starting with capital letter. Hyphen alowed',
-    textSurname: 'Please write a surname in latin 4 letters minimum starting with capital letter. Hyphen alowed',
+    textName: 'The name can contain only Latin letters or a hyphen. At least 3 characters that start with capital letter',
+    textSurname: 'Please write a surname in latin 4 letters minimum starting with capital letter.',
 };
 
-export default function createLoginForm(): HTMLFormElement {
-    const nameBlock = new BaseHTMLElementClass('div', [] );
-    const nameLabel = new BaseHTMLElementClass('label', [], 'Name:');
-    const nameInput = new BaseHTMLElementClass('input', ['name-input']);
-    const errorNameBlock = new BaseHTMLElementClass('div', ['error-msg'], errorMsg.textName);
-    nameBlock.appendChilds([nameLabel.getElement(), nameInput.getElement(),errorNameBlock.getElement()])
-    
-    const surnameBlock = new BaseHTMLElementClass('div', [] );
-    const surnameLabel = new BaseHTMLElementClass('label', [], 'Surname:');
-    const surnameInput = new BaseHTMLElementClass('input', ['name-input']);
-    const errorSurnameBlock = new BaseHTMLElementClass('div', ['error-msg'], errorMsg.textSurname );
-    surnameBlock.appendChilds([surnameLabel.getElement(), surnameInput.getElement(),errorSurnameBlock.getElement()]);
+type nameObj = {nameBlock:HTMLFormElement,nameInput:HTMLFormElement,errorBlock:BaseHTMLElementClass}
 
+function createNameInputBlock(nameStr:string): nameObj{
+
+    const nameBlock = new BaseHTMLElementClass('div', [] );
+    const nameLabel = new BaseHTMLElementClass('label', [], nameStr);
+    const nameInput = new BaseHTMLElementClass('input', ['name-input']);
+    const errorNameBlock = new BaseHTMLElementClass('div', ['error-msg']);
+    nameBlock.appendChilds([nameLabel.getElement(), nameInput.getElement(),errorNameBlock.getElement()]);
+    return {nameBlock:nameBlock.getElement() as HTMLFormElement,nameInput:nameInput.getElement() as HTMLFormElement,errorBlock:errorNameBlock as BaseHTMLElementClass};
+
+}
+
+export default function createLoginForm(): HTMLFormElement {
+    
+    const nameBlockObj:nameObj = createNameInputBlock('Name:');
+    const surnameBlockObj:nameObj = createNameInputBlock('Surname:');
+    
     const submitButton = new BaseHTMLElementClass('button', ['submit-button'], 'Login');
 
     const form = new BaseHTMLElementClass('div', ['login-form']);
-    form.appendChilds([nameBlock.getElement(), nameInput.getElement(), surnameBlock.getElement(), submitButton.getElement()]);
+    form.appendChilds([nameBlockObj.nameBlock, surnameBlockObj.nameBlock, submitButton.getElement()]);
 
     submitButton.addEventListener('click', (event: Event) => {
-        event.preventDefault(); // Prevent default form submission behavior
-        
-        const name = (nameInput.getElement() as HTMLInputElement).value;
-        const surname = (surnameInput.getElement() as HTMLInputElement).value;
+        event.preventDefault(); 
+        const name = (nameBlockObj.nameInput).value;
+        const surname = (surnameBlockObj.nameInput).value;
 
-        if(validateInput(name,2) && validateInput(surname,3)){
+        if(validateInput(name,2)){
             console.log(name);
             console.log(surname);
-            
+            nameBlockObj.errorBlock.setTextContent('');
         }else{
-            console.log('Invalid input. Name and surname must start with capital letters and have a minimum length of 3 characters.');
+            
+            nameBlockObj.errorBlock.setTextContent(errorMsg.textName);
+        }
+
+        if(validateInput(surname,3)){
+            surnameBlockObj.errorBlock.setTextContent('');
+        }else{
+            surnameBlockObj.errorBlock.setTextContent(errorMsg.textSurname);
         }
   
     });
