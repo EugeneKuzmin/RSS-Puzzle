05e7a40a6b0867fab5a59575ef0a1558b5af365e
Gennarro@yandex.ru <Gennarro@yandex.ru>
2024-03-18 13:47:18 +0300

    feat:setand adjust markup properly

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -2,28 +2,31 @@ import './LoginStyles.css';
 import validateInput from '../utils/validation'
 import BaseHTMLElementClass from '../BaseHTMLElementClass';
 const errorMsg = {
-    textName: 'The name can contain only Latin letters or a hyphen. At least 3 characters that start with capital letter',
-    textSurname: 'Please write a surname in latin 4 letters minimum starting with capital letter.',
+    textName: 'The name can contain only Latin letters or a hyphen. At least 3 characters that start with capital letter.',
+    textSurname: 'The surname can contain only Latin letters or a hyphen. At least 4 characters that start with capital letter.',
 };
 
-type nameObj = {nameBlock:HTMLFormElement,nameInput:HTMLFormElement,errorBlock:BaseHTMLElementClass}
+type nameObj = {nameBlock:HTMLFormElement,nameInput:HTMLFormElement,nameInputElement:BaseHTMLElementClass,errorBlock:BaseHTMLElementClass}
 
 function createNameInputBlock(nameStr:string): nameObj{
 
-    const nameBlock = new BaseHTMLElementClass('div', [] );
+    const nameBlock = new BaseHTMLElementClass('div', ['name-container'] );
+
+    const labelBlock = new BaseHTMLElementClass('div', [] );
     const nameLabel = new BaseHTMLElementClass('label', [], nameStr);
+    labelBlock.appendChilds([nameLabel.getElement()]);
+
     const nameInput = new BaseHTMLElementClass('input', ['name-input']);
     const errorNameBlock = new BaseHTMLElementClass('div', ['error-msg']);
-    nameBlock.appendChilds([nameLabel.getElement(), nameInput.getElement(),errorNameBlock.getElement()]);
-    return {nameBlock:nameBlock.getElement() as HTMLFormElement,nameInput:nameInput.getElement() as HTMLFormElement,errorBlock:errorNameBlock as BaseHTMLElementClass};
 
+    nameBlock.appendChilds([labelBlock.getElement(), nameInput.getElement(),errorNameBlock.getElement()]);
+    return {nameBlock:nameBlock.getElement() as HTMLFormElement,nameInput:nameInput.getElement() as HTMLFormElement,nameInputElement:nameInput as BaseHTMLElementClass,errorBlock:errorNameBlock as BaseHTMLElementClass};
 }
 
 export default function createLoginForm(): HTMLFormElement {
     
     const nameBlockObj:nameObj = createNameInputBlock('Name:');
     const surnameBlockObj:nameObj = createNameInputBlock('Surname:');
-    
     const submitButton = new BaseHTMLElementClass('button', ['submit-button'], 'Login');
 
     const form = new BaseHTMLElementClass('div', ['login-form']);
@@ -35,18 +38,17 @@ export default function createLoginForm(): HTMLFormElement {
         const surname = (surnameBlockObj.nameInput).value;
 
         if(validateInput(name,2)){
-            console.log(name);
-            console.log(surname);
             nameBlockObj.errorBlock.setTextContent('');
         }else{
-            
             nameBlockObj.errorBlock.setTextContent(errorMsg.textName);
+            nameBlockObj.nameInputElement.addClass('error');
         }
-
+        
         if(validateInput(surname,3)){
             surnameBlockObj.errorBlock.setTextContent('');
         }else{
             surnameBlockObj.errorBlock.setTextContent(errorMsg.textSurname);
+            surnameBlockObj.nameInputElement.addClass('error');
         }
   
     });
