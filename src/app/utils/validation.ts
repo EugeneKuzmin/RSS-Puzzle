5fc7725dc8f4d13ae3331564f42549ee58e0de68
EugeneKuzmin <81953602+EugeneKuzmin@users.noreply.github.com>
2024-03-18 14:05:14 +0300

    Revert "Rss pz 02 field validation (#2)"
    This reverts commit 3114747c25bde887b5ef3815b39d0eeac7a60cc3.

--- src/app/utils/validation.ts
+++
@@ -1,4 +0,0 @@
-export default function validateInput(input: string,nameLength:number): boolean {
-    const regex = new RegExp(`^[A-Z][a-z-]{${nameLength-1},}[a-z]$`);
-    return regex.test(input);
-}
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,26 @@
-import createLoginForm from './app/login/login';
+import "./style.css";
+import typescriptLogo from "./typescript.svg";
+import viteLogo from "../../../../../../vite.svg";
+import { setupCounter } from "./counter.ts";
 
-const form = createLoginForm();
+const val: number = 5;
 
-document.body.appendChild(form);
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://www.typescriptlang.org/" target="_blank">
+      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+    </a>
+    <h1>Vite + TypeScript</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite and TypeScript logos to learn more
+    </p>
+  </div>
+`;
+
+setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
