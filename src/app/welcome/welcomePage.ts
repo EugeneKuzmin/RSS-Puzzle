339854cb4921c447b4149e1a7c40d84476e9d805
EugeneKuzmin <81953602+EugeneKuzmin@users.noreply.github.com>
2024-03-19 19:33:20 +0300

    feat:add user greeting (#11)

--- src/app/welcome/welcomePage.ts
+++ src/app/welcome/welcomePage.ts
@@ -1,21 +1,49 @@
 import './welcomeStyles.css';
 import BaseHTMLElementClass from '../BaseHTMLElementClass.ts';
+import ImageManager from '../components/img/imgClass.ts';
+import LocalStorageManager from '../utils/localStorageWorkflow.ts';
 
 const ABOUT_GAME =
   'Learn English with our fun game of juggling puzzles and making sentences. You need to place words in the proper order from the given words and make a correct sentence.';
 export default function createWelcomeForm(): HTMLFormElement {
   const gameName = new BaseHTMLElementClass(
     'div',
-    ['flex', 'justify-content-center', 'game-name', 'caprasimo-regular'],
+    ['text-center', 'game-name', 'caprasimo-regular'],
     'RSS Puzzle'
   );
   const gameDescription = new BaseHTMLElementClass(
     'div',
-    ['flex', 'justify-content-center', 'caprasimo-regular', 'game-description'],
+    ['text-center', 'caprasimo-regular', 'game-description'],
     ABOUT_GAME
   );
+
+  const playersName = LocalStorageManager.get('rss_puzzle__user');
+
+  const welcomeGreeting = new BaseHTMLElementClass(
+    'div',
+    ['text-center', 'greeting-welcome'],
+    'Welcome'
+  );
+
+  const personalGreeting = new BaseHTMLElementClass(
+    'div',
+    ['text-center', 'greeting-name'],
+    `${playersName.name} ${playersName.surname}`
+  );
+
+  // const imageManager = new ImageManager(
+  //   ['image-class'],
+  //   '../../assets/44d1160cd4dfc914270ba22f2edcf2cf.png',
+  //   'puzzle game'
+  // );
   const form = new BaseHTMLElementClass('div', ['welcome-form']);
-  form.appendChilds([gameName.getElement(), gameDescription.getElement()]);
+  form.appendChilds([
+    gameName.getElement(),
+    gameDescription.getElement(),
+    welcomeGreeting.getElement(),
+    personalGreeting.getElement()
+    // imageManager.getElement()
+  ]);
 
   return form.getElement() as HTMLFormElement;
 }
